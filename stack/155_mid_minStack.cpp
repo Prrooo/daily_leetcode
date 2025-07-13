8b297896bd3aa30682553105bc1b74a795b7af8a
Prrooo <harshmalik00011@gmail.com>
2025-07-14 01:54:23 +0530

    155_mid -rev

---
+++ stack/155_mid_minStack.cpp
@@ -0,0 +1,103 @@
+// Design a stack that supports push, pop, top, and retrieving the minimum
+// element in constant time.
+//
+// Implement the MinStack class:
+//
+//     MinStack() initializes the stack object.
+//     void push(int val) pushes the element val onto the stack.
+//     void pop() removes the element on the top of the stack.
+//     int top() gets the top element of the stack.
+//     int getMin() retrieves the minimum element in the stack.
+//
+// You must implement a solution with O(1) time complexity for each function.
+//
+//
+//
+// Example 1:
+//
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+//
+// Output
+// [null,null,null,null,-3,null,0,-2]
+//
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// best solution
+
+class MinStack {
+private:
+  vector<vector<int>> st;
+
+public:
+  MinStack() {}
+
+  void push(int val) {
+    int min_val = getMin();
+    if (st.empty() || min_val > val) {
+      min_val = val;
+    }
+    st.push_back({val, min_val});
+  }
+
+  void pop() { st.pop_back(); }
+
+  int top() { return st.empty() ? -1 : st.back()[0]; }
+
+  int getMin() { return st.empty() ? -1 : st.back()[1]; }
+};
+
+// work fine but neet imporvement
+
+class MinStack2 {
+public:
+  map<int, int> mp;
+  vector<int> arr;
+  MinStack2() {}
+
+  void push(int val) {
+    arr.push_back(val);
+    mp[val]++;
+  }
+
+  void pop() {
+    int size = arr.size();
+    if (size == 0)
+      return;
+    mp[arr[size - 1]]--;
+    if (mp[arr[size - 1]] == 0)
+      mp.erase(arr[size - 1]);
+    arr.pop_back();
+  }
+
+  int top() { return arr[arr.size() - 1]; }
+
+  int getMin() {
+    auto top = mp.begin();
+    return top->first;
+  }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+int main() { return 0; }
