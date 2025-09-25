66476e570e8d405ae63e596f28802fb5bbc864b8
Prrooo <harshmalik00011@gmail.com>
2025-09-25 12:04:22 +0530

    .

---
+++ String/166_mid_FractionToRecurringDecimal.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  string fractionToDecimal(int num, int den) {
+    double fract = (double)num / den;
+    cout << fract << endl;
+    return "";
+  }
+};
+
+int main() { return 0; }
